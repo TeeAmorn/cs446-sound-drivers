9e07ae45da31ef8408b4754d61b2ac2cb6a74ead
Kyle Nolan Williams <knw5782@moore.wot.eecs.northwestern.edu>
2023-05-04 14:52:37 -0500

    AC97 Registers Encoded with macros

---
+++ include/dev/ac97_pci.h
@@ -0,0 +1,36 @@
+/*
+ * This file is part of the Nautilus AeroKernel developed
+ * by the Hobbes and V3VEE Projects with funding from the
+ * United States National  Science Foundation and the Department of Energy.
+ *
+ * The V3VEE Project is a joint project between Northwestern University
+ * and the University of New Mexico.  The Hobbes Project is a collaboration
+ * led by Sandia National Laboratories that includes several national
+ * laboratories and universities. You can find out more at:
+ * http://www.v3vee.org  and
+ * http://xtack.sandia.gov/hobbes
+ *
+ * Copyright (c) 2017, Panitan Wongse-ammat
+ * Copyright (c) 2017, Peter Dinda
+ * Copyright (c) 2017, The V3VEE Project  <http://www.v3vee.org>
+ *                     The Hobbes Project <http://xstack.sandia.gov/hobbes>
+ * All rights reserved.
+ *
+ * Authors: Panitan Wongse-ammat <Panitan.W@u.northwesttern.edu>
+ *          Marc Warrior <warrior@u.northwestern.edu>
+ *          Galen Lansbury <galenlansbury2017@u.northwestern.edu>
+ *          Peter Dinda <pdinda@northwestern.edu>
+ *
+ * This is free software.  You are permitted to use,
+ * redistribute, and modify it as specified in the file "LICENSE.txt".
+ */
+
+#ifndef __AC97_PCI
+#define __AC97_PCI
+
+int ac97_pci_init(struct naut_info *naut);
+int ac97_pci_deinit();
+
+// TODO: Add more functions from ac97_pci.c as necessary
+
+#endif
